508d27383503efefb4c35ff49c77dd6cbabe455a
Ricardo Braga <rcardiobraga@gmail.com>
2020-09-17 19:53:01 -0300

    Criando pagina de listagem dos registros

---
+++ front-web/src/pages/Records/types.ts
@@ -0,0 +1,16 @@
+export type RecordResponse = {
+    content: RecordItem[];
+    totalPages: number;
+}
+
+export type RecordItem  = {
+    id: number;
+    moment: string;
+    name: string;
+    age: number;
+    gameTitle: string;
+    gamePlatform: Platform;
+    genreName: String;
+}
+
+export type Platform = 'XBOX' | 'PC' | 'PLAYSTATION';
